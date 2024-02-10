327132c7b8e96704fc62bce52a6f4bef920bc5bf
Fabio53443 <fabiomassimoproietti1@gmail.com>
2024-02-10 18:54:28 +0100

    fixed speed ctrl

--- main.py
+++ main.py
@@ -11,15 +11,21 @@ def setup():
     motorHandles[1] = sim.getObject('./front_right_wheel')
     motorHandles[2] = sim.getObject('./back_right_wheel')
     motorHandles[3] = sim.getObject('./back_left_wheel')
+    cam = sim.getObject('./kinect/rgb')
     return motorHandles, sim
-def reset():
+
+def stop():
     setSpeed4W(0, 0, 0, 0, 0)
 
+
+
+
 def setSpeed4W(fl, fr, bl, br, time):
+    fl, fr, bl, br = fl*10, fr*10, bl*10, br*10
     m, sim = setup()
     sim.setJointTargetVelocity(m[0], fl)
-    sim.setJointTargetVelocity(m[1], fr)
-    sim.setJointTargetVelocity(m[2], br)
+    sim.setJointTargetVelocity(m[1], -fr)
+    sim.setJointTargetVelocity(m[2], -br)
     sim.setJointTargetVelocity(m[3], bl)
     sleep(time)
     sim.setJointTargetVelocity(m[0], 0)
@@ -48,8 +54,4 @@ def sinistra(speed, time):
     setSpeed4W(sxSpeed, speed, sxSpeed, speed, time)
 
 
-#reset()
-#speed = random.randint(10, 100)
-#time = random.randint(10, 100)
-
-sinistra(1, 2)
+setup()
